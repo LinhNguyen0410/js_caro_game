747ed9a5f62b7f71b4763b07014a4c8627107e37
Linh Nguyen <linhgbale0410@gmail.com>
2021-12-06 13:49:05 +0700

    js_caro

---
+++ js/selectors.js
@@ -0,0 +1,21 @@
+// Implement selector function to get elements needed
+// 1. Cell List
+// 2. Current Turn
+// 3. Replay Game
+// 4. Game status
+// viết các hàm get element riêng đề khi mình muốn sử dụng ở nhiều nơi thì mình chỉ cần gọi hàm mà k cần get bằng việc gõ document lặp lại code nữa
+
+export function getCellElementList() {
+  return document.querySelectorAll("#cellList > li");
+}
+
+export function getCurrentTurnElement() {
+  return document.querySelector("#currentTurn");
+}
+export function getCellElementAtIdx(index) {
+  // do index trong arr bắt đầu từ 0 mà thăng nth-child trong css bắt đầu từ 1 nên mình mới +1
+  return document.querySelectorAll(`#cellList > li:nth-child( ${index + 1} )`);
+}
+export function getGameStatusElement() {
+  return document.querySelector("#gameStatus");
+}
